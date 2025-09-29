3d22d6ed379e9c0fff491830884e37d11f04b37a
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-09-29 21:46:03 +0530

    Add files via upload

---
+++ Day-3 Practice/Richest_Customer_wealth.java
@@ -0,0 +1,17 @@
+class Richest_Customer_wealth {
+    public int maximumWealth(int[][] accounts) {
+        int sum = 0;
+        int ans = 0;
+        for(int i=0; i<accounts.length; i++){
+            for(int j=0; j<accounts[0].length; j++){
+                sum += accounts[i][j];
+
+               
+            
+            }
+             ans = Math.max(ans, sum);
+            sum = 0;
+        }
+        return ans;
+    }
+}
