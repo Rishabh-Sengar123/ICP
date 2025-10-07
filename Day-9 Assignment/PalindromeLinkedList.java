b277079efdf7974c61d22db4aaf7b2aa649b6f58
Rishabh Sengar <142567797+Rishabh-Sengar123@users.noreply.github.com>
2025-10-07 21:53:11 +0530

    Add files via upload

---
+++ Day-9 Assignment/PalindromeLinkedList.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode prev = null, curr = slow, next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        ListNode first = head, second = prev;
+        while (second != null) {
+            if (first.val != second.val) return false;
+            first = first.next;
+            second = second.next;
+        }
+        return true;
+    }
+}
